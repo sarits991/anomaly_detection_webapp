80dbcb4e087cc143da2f18c81fa1d1e9d6debc87
sarits991 <you@example.com>
2021-05-21 18:52:37 +0300

    finish web client

--- controller/control.js
+++ controller/control.js
@@ -4,13 +4,19 @@ const anomalyHandler = require('../build/Release/anomaly_detection_addon');
 exports.findAnomalies = async (req, res) => {
     let type = req.body['model_type'];
     if(typeof type == "undefined") {
-        res.status(400).send("invalid data");
+        res.status(400).send({error:"invalid data"});
         return;
     }
     if(type.localeCompare("hybrid") && type.localeCompare("regression")) {
-        res.status(400).send("invalid model type");
+        res.status(400).send({error:"invalid model type"});
         return;
     }
+
+    if(typeof req.files.train_file == "undefined" || typeof req.files.detect_file == "undefined" ){
+        res.status(400).send({error:"invalid files"});
+        return;
+    }
+
     let train_file = String(req.files.train_file.data).replace(/(\r\n)/g, "\n");
     let detect_file = String(req.files.detect_file.data).replace(/(\r\n)/g, "\n");
     let files = modelHandler.createFiles(train_file, detect_file);
---
+++ view/post_response_handles.js
@@ -0,0 +1,23 @@
+function OnLoad() {
+    let template = {'<>': 'ul','html':'<li><p style="font-size:17px;">feature1: ${feature1} <br>feature2: ${feature2} <br> timesteps: ${range}</p> </li>'};
+    let templateError = {'<>': 'div','html':'<p style="color: red;font-size:20px;"> ${error}</p>'};
+    let iframe = document.getElementById("result");
+    let data = iframe.contentDocument.body.innerText;
+    if(data == null || typeof data == "undefined" || data == "")
+        return;
+    let json = JSON.parse(data);
+    if(json.hasOwnProperty('error')){
+        iframe.contentDocument.write(json2html.render(data,templateError));
+    }
+    else{
+        iframe.contentDocument.write( json2html.render(data,template));
+    }
+}
+
+function pending() {
+    let iframe = document.getElementById("result");
+    let text  = iframe.contentDocument.body;
+    if(text != null)
+        text.innerHTML = "";
+    iframe.contentDocument.write("pending..");
+}
