ddc39e806238c2475cd8c079c01c3062373d3066
sarits991 <you@example.com>
2021-05-22 23:03:02 +0300

    create directory and app.js

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.use(express.static('view'))
 app.use(fileUpload({}))
 app.use('/', userRoutes);
 
+let fs = require('fs');
+let dir = "csv_files"
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
 app.listen(8080, () => {
     console.log("Server running on port 8080");
 });
